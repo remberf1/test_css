28babbe6526a9b031acab64d0e9f12c7d86dbcb2
remberf1 <68003095+remberf1@users.noreply.github.com>
2025-07-11 20:45:36 +0100

    resposive

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/users/new',
+        permanent: false, // or true if you want a permanent redirect
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
