2f2478622ad2ef29e71013516a004e4a6af4f36f
remberf1 <68003095+remberf1@users.noreply.github.com>
2025-07-12 22:57:47 +0100

    fixed

--- app/users/Blog_preview/page.tsx
+++ app/users/Blog_preview/page.tsx
@@ -8,7 +8,7 @@ const text=['Github','Frontend Mentor','Linkedin','Twitter','Instagram']
 const inter = Inter({ subsets: ['latin'] })
 const Preview = () => {
   return (
-        <div className={`${inter.className} bg-[var(--color-Grey-900)] min-h-screen py-5 flex justify-center`}>
+        <div className={`${inter.className} bg-[var(--color-Grey-900)] min-h-screen py-1 flex justify-center`}>
   <div className="w-full max-w-sm px-8 py-10 flex flex-col items-center justify-center text-center rounded-xl bg-Grey-800 space-y-4">
     <Image src="/avatar-jessica.jpeg" height={80} width={80} alt="Avatar" className="rounded-full" />
     <div>
